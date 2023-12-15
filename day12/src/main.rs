1f958637ffaa9ca1c18f5ff9f9a9fa9cc53ff4bf
Edward J. Schwartz <eschwartz@cert.org>
2023-12-14 20:20:43 -0500

    finally complete day12 :-(

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -2,7 +2,9 @@ use std::collections::VecDeque;
 
 use indicatif::ProgressIterator;
 
-#[derive(Debug, Clone)]
+use memoize::memoize;
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 struct Config {
     conditions: Vec<char>,
     broken: Vec<usize>,
@@ -116,143 +118,79 @@ impl Config {
         }
     }
 
-    fn test2(&self) -> usize {
-
-        if self.conditions.len() == 0 {
-            return if self.broken.is_empty() {1} else {0};
-        }
-
-        dbg!(&self.conditions, &self.broken);
-        // So the idea is to assume that we're always starting at a new
-        // contiguous segment of broken springs.
-
-        // So we'll look and see where the next . is.  Let's say it's at index
-        // i.  Every character in [0, i) is either a ? or #.  We'll consider
-        // what would happen for each position that is a ?, since this could
-        // terminate the contiguous chunk.
-
-        let next_period =
-            self.conditions
-                .iter()
-                .enumerate()
-                //.skip(1) // is this right?
-                .find_map(|(i, c)| if *c == '.' { Some(i) } else { None });
-
-        let next_broken = self.broken.first();
-
-        let period_index = next_period.unwrap_or(self.conditions.len()-1);
-
-        println!("period_index is {period_index}. this means that we'll consider all the possibilities in 0..{period_index} and then recurse.");
-
-        let mut choices = 1_usize;
-
-        for (i, c) in self.conditions[0..=period_index].iter().enumerate() {
-
-            // In this loop, we are now considering (i-1)*#.
-            // We are now considering i-1*#
-
-            dbg!(&i);
-
-            //dbg!(i,c,choices);
-
-            match *c {
-                '#' => (), // It's not possible to end here.
-                '.' => {
-                    // We're ending here.  We better match!
-                    // We have i-1 #.  
-                    if i > 0 && Some(&(i-1)) == next_broken {
-                        // OK... recurse on the remainder                        
-                        let config = Config{
-                            conditions: self.conditions[i+1..].to_vec(),
-                            broken: self.broken[1..].to_vec() // tail
-                        };
-                        println!("recursing on the remainder {:?}", config);
-                        choices *= config.test2();
-                    } else {
-                        println!("Um this is bad");
-                        return 0
-                    }
-                }
-                '?' => {
-
-                    //dbg!(Some(&i), next_broken);
-
-                    // What if it's a period? ###.
-                    dbg!(i);
-                    if (i > 0 && Some(&(i-1)) == next_broken) || i == 0 {
-                        dbg!(i);
-                        // i == 0 because we don't have any springs yet.
-                        // It is possible for ? to be a .
-                        // Recurse on [i..]
-                        println!("We could break on a ? at {i}!");
-                        println!("{} {period_index}", i+1);
-                        if i+1 <= period_index {
-                            let recurse_conditions = self.conditions[i+1..period_index].to_vec();
-                            let broken = if i == 0 {
-                                self.broken.clone()
-                            } else {
-                                self.broken[1..].to_vec()
-                            };
-                            let config = Config{
-                                conditions: recurse_conditions,
-                                broken // this is .tail
-                            };
-                            println!("add recursing on {:?}", config);
-                            choices += dbg!(config.test2());
-                        }
-                    } else {
-                        println!("Can't put . here at {i}");
-                    }
-
-                    // What if it's a #? Then we continue
-                },
-                _ => panic!("unknown char")
-            }
-        }
-
-        choices
-    }
-
-    /*
-    fn test(&self) -> usize {
-        // Generalization:
-
-        let first_question =
-            self.conditions
-                .iter()
-                .enumerate()
-                .find_map(|(i, c)| if *c == '?' { Some(i) } else { None });
-
-        match (self.conditions.as_slice(), self.broken.as_slice()) {
-            // We need to match at least one broken spring, and there are no places for them to go.
-            ([], [_, ..]) => 0,
-            (['.', ..], _) => {
-                let new = Config {
-                    conditions: self.conditions[1..].to_vec(),
-                    broken: self.broken.clone()
-                };
-                new.test()
-            },
-            (['#', ..], [n, ..]) => {
-                let am_i_new_contiguous =
-            }
-            _ => panic!("tbd")
-        }
-    }
-    */
 }
+
+#[memoize]
+fn rec_attempt(c: Config, num_broken_pre: usize) -> usize {
+
+    //dbg!(&self.conditions);
+
+    match c.conditions.first() {
+        None => {
+            match (c.broken.as_slice(), num_broken_pre) {
+                ([x], y) if *x == y => 1,
+                ([0], 0) => 1,
+                ([], 0) => 1,
+                _ => 0
+            }
+        },
+        Some('#') => {
+            let new_conditions = c.conditions[1..].to_vec();
+            rec_attempt(Config{conditions: new_conditions, broken: c.broken.clone()}, num_broken_pre + 1)
+        },
+        Some('.') => match (c.broken.as_slice(), num_broken_pre) {
+            ([x, tail @ ..], y) if *x == y => {
+
+                // Cool, let's consume it and recurse.
+                let new_broken = c.broken[1..].to_vec();
+                let new_conditions = c.conditions[1..].to_vec();
+                rec_attempt(Config{
+                    conditions: new_conditions,
+                    broken: new_broken
+                }, 0)
+            },
+            (_, 0) => {
+                // Don't consume but recurse
+                let new_conditions = c.conditions[1..].to_vec();
+                rec_attempt(Config {
+                    conditions: new_conditions,
+                    broken: c.broken.clone()
+                }, 0)
+            },
+            _ => 0 // nope
+        },
+        Some('?') => {
+            // Well, try each option.
+
+            [".", "#"].iter().map(|s| {
+                let new_conditions = s.chars().chain(c.conditions[1..].to_vec().into_iter()).collect::<Vec<_>>();
+                rec_attempt(Config{conditions: new_conditions, broken: c.broken.clone()}, num_broken_pre)
+            }).sum()
+        },
+        Some(_) => panic!("what")
+    }
+}
+
 
 fn main() {
     let configs = from_stdin(std::io::stdin());
     let configs: Vec<_> = configs.collect();
     //dbg!(&c);
 
-    let counts = configs
+/*      let counts = configs
         .iter()
         .map(|c| c.expand().filter(|c| c.is_valid()).count())
         .progress()
         .collect::<Vec<_>>();
-    println!("Part 1: {}", counts.iter().sum::<usize>());
+    println!("Part 1: {}", counts.iter().sum::<usize>());*/
+
+    let counts_new = configs
+        .iter()
+        .map(|c| rec_attempt(c.clone(), 0))
+        .progress()
+        .collect::<Vec<_>>();
+
+    dbg!(&counts_new);
 
     /*
     for c in configs {
@@ -266,17 +204,18 @@ fn main() {
 
     let p2_configs = configs.iter().map(|c| c.to_part2()).collect::<Vec<_>>();
 
-    for c in configs.iter() {
-        println!("hmm {}", c.test2());
-    }
+    let p2_counts = p2_configs
+        .iter()
+        .map(|c| rec_attempt(c.clone(), 0))
+        .progress()
+        .collect::<Vec<_>>();
 
     /*let p2_counts = p2_configs
-        .iter()
-        .progress()
-        .map(|c| c.expand().filter(|c| c.is_valid()).count())
-        .collect::<Vec<_>>();
-*/
+            .iter()
+            .progress()
+            .map(|c| c.expand().filter(|c| c.is_valid()).count())
+            .collect::<Vec<_>>();
+    */
 
-
-    //println!("Part 2: {}", p2_counts.iter().sum::<usize>());
+    println!("Part 2: {}", p2_counts.iter().sum::<usize>());
 }
